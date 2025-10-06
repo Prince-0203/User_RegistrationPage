ce44025f50d52186d93d00d50f3a35c2c5496393
Prince-0203 <tearfultunes78018@gmail.com>
2025-10-06 09:15:25 +0530

    Add Method commit

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Students}/{action=Index}/{id?}");
+    pattern: "{controller=Students}/{action=Register}/{id?}");
 
 app.Run();
